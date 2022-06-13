8c21832ef63b3dbabd9c3bc0bdd708b9b6749c6d
moemti <56967949+moemti@users.noreply.github.com>
2022-06-13 12:20:39 +0300

    dupa lugoj

--- config/navigation.php
+++ config/navigation.php
@@ -20,7 +20,8 @@ return  [
                                 [
                                         ['gallery/13', 'Etapa 1 Ludus 19,20-03-2022'], 
                                         ['gallery/14', 'Etapa 2 Lugoj 9,10-04-2022'],
-                                        ['gallery/15', 'Etapa 2 Bucuresti 21,22-05-2022']
+                                        ['gallery/15', 'Etapa 3 Bucuresti 21,22-05-2022'],
+                                        ['gallery/16', 'Etapa 4 Lugoj 11,12-06-2022'],
                 
                                 ], 'Galerii foto'
                 
