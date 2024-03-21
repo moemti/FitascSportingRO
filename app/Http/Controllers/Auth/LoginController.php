ae522fa2df956af241ea95495ceb62165988d616
moemti <56967949+moemti@users.noreply.github.com>
2024-03-21 23:45:28 +0200

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,120 +2,96 @@
 
 namespace App\Http\Controllers\Auth;
 
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
 use App\Http\Controllers\Controller;
-
 use App\Models\Common\Login;
-
-use App\Models\Dictionaries\Dictionary;
-use App\Models\Users\User;
-use App\Models\Users\Person;
-use App\Models\Users\UserPerson;
-use App\Models\Common\Utilities;
 use App\Models\Competitions\Competition;
+use App\Models\Dictionaries\Dictionary;
+use App\Models\Users\Person;
+use App\Models\Users\User;
+use App\Models\Users\UserPerson;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-
-
+use Illuminate\Support\Facades\Redirect;
 
 class LoginController extends Controller
 {
-    public function login(){
-        
-        return view("auth/login")->with(["mesaj"=>["OK"]]);
+    public function login()
+    {
+        return view('auth/login')->with(['mesaj' => ['OK']]);
     }
-    
-    
-    public function changepassword(Request $request){
 
+    public function changepassword(Request $request)
+    {
         $token = $request->token;
-        
-        return view("auth/changepassword", ['_passtoken'=>$token]);
+        return view('auth/changepassword', ['_passtoken' => $token]);
     }
 
-    public function changethepassword(Request $request){
-
+    public function changethepassword(Request $request)
+    {
         $password2 = $request->password2;
 
         $password = $request->password;
+        $passtoken = $request->_passtoken;
 
-        $passtoken = $request-> _passtoken;
-
-        if ($password !== $password2)
-            return view("auth/changepassword")->with(["mesaj" => ['mesaj' =>transex('Ai introdus parole diferite. Incearca inca o data')], "_passtoken"=>$passtoken]);    
+        if ($password !== $password2) {
+            return view('auth/changepassword')->with(['mesaj' => ['mesaj' => transex('Ai introdus parole diferite. Incearca inca o data')], '_passtoken' => $passtoken]);
+        }
 
         $password = crypt($password, $password);
         $message = User::setPassword($password, $passtoken);
 
-        if ($message == 'OK')
-            return view("auth/login")->with(["mesaj" => ['mesaj' => transex('Parola a fost modificata!')]]);
-        else
-            return view("auth/changepassword")->with(["mesaj" => ['mesaj' =>$message], "_passtoken"=>$passtoken]);
-
+        if ($message == 'OK') {
+            return view('auth/login')->with(['mesaj' => ['mesaj' => transex('Parola a fost modificata!')]]);
+        } else {
+            return view('auth/changepassword')->with(['mesaj' => ['mesaj' => $message], '_passtoken' => $passtoken]);
+        }
     }
-    
-    
-    public function resetpasswordmail(Request $request){
+
+    public function resetpasswordmail(Request $request)
+    {
         // reset the password
-        
-       
+
         // if email exists ok and back to intex
         $Email = $request->input('email');
-        
-        $user = Login::EmailExists($Email);
-        
-        if (isset($user) && count($user) > 0) {
 
+        $user = Login::EmailExists($Email);
+
+        if (isset($user) && count($user) > 0) {
             $passtoken = uniqid();
 
             User::setPasswordReset($Email, $passtoken);
 
-
             $data = [
                 'title' => transex('Modificare parola'),
-                'content' => transex("Apasa linkul pentru a modifica parola"),
-                'link' => url('/changepassword?token='.$passtoken),
-
+                'content' => transex('Apasa linkul pentru a modifica parola'),
+                'link' => url('/changepassword?token=' . $passtoken),
             ];
-        
-            Mail::send('mails.resetmail', $data, function($message) use ($Email){
 
+            Mail::send('mails.resetmail', $data, function ($message) use ($Email) {
                 $message->to($Email, 'User')->from('noreply@fitascsporting.ro')->subject(transex('Modificare parola'));
-            }
-
-            );
-            return view("auth/login")->with(["mesaj" =>['password'=> transex('S-a trimis un email de confirmare catre adresa introdusa. Trebuie sa confirmi apasand link-ul din email!')]]);
+            });
+            return view('auth/login')->with(['mesaj' => ['password' => transex('S-a trimis un email de confirmare catre adresa introdusa. Trebuie sa confirmi apasand link-ul din email!')]]);
+        } else {
+            return view('auth/reset')->with(['mesaj' => ['email' => transex('Nu exista utilizator cu emailul introdus!')]]);
         }
-        else
-            return view("auth/reset")->with(["mesaj" =>['email'=> transex('Nu exista utilizator cu emailul introdus!')]]);
-            
-            
-            
     }
-    
-    public function register(Request $request){
 
+    public function register(Request $request)
+    {
         $password2 = $request->password2;
-
         $password = $request->password;
-
         $Email = $request->Email;
 
-
-        if ($password !== $password2)
-            return view("auth/register")->with(["mesaj" => ['NotOK' =>transex('Ai introdus parole diferite. Incearca inca o data')]]);    
+        if ($password !== $password2) {
+            return view('auth/register')->with(['mesaj' => ['NotOK' => transex('Ai introdus parole diferite. Incearca inca o data')]]);
+        }
 
         $password = crypt($password, $password);
-
         $request['Password'] = $password;
-
-
-
         $user = Login::EmailExists($Email);
 
         if (isset($user) && count($user) > 0) {
-            return view("auth/register")->with(["mesaj" => ['NotOK' =>transex('Exista deja un utilizator cu acest email!')]]);
+            return view('auth/register')->with(['mesaj' => ['NotOK' => transex('Exista deja un utilizator cu acest email!')]]);
         }
 
         $passtoken = uniqid();
@@ -124,34 +100,29 @@ class LoginController extends Controller
 
         $data = [
             'title' => transex('Inregistrare pe fitascsporting.ro'),
-            'content' => transex("Apasa linkul pentru a confirma emailul. Dupa confirmare se va analiza si crea utilizatorul. Veti primi un alt email de confirmare a utilizatorului!"),
-            'link' => url('/confirmregistration?token='.$passtoken),
-
+            'content' => transex('Apasa linkul pentru a confirma emailul. Dupa confirmare se va analiza si crea utilizatorul. Veti primi un alt email de confirmare a utilizatorului!'),
+            'link' => url('/confirmregistration?token=' . $passtoken),
         ];
 
-        $message =  UserPerson::registeruser($request->all());
+        $message = UserPerson::registeruser($request->all());
 
-      
+        if ($message == 'OK') {
+            Mail::send('mails.registration', $data, function ($message) use ($Email) {
+                $message->to($Email, 'User')->from('noreply@fitascsporting.ro')->subject(transex('Inregistrare pe fitascsporting.ro'));
+            });
 
+            Mail::send('mails.registration', $data, function ($message) use ($Email) {
+                $message->to('admin@fitascsporting.ro', 'User')->from('noreply@fitascsporting.ro')->subject(transex('Inregistrare pe fitascsporting.ro'));
+            });
 
-        if ($message == 'OK'){
-
-            Mail::send('mails.registration', $data, function($message) use ($Email){
-
-                    $message->to($Email, 'User')->from('noreply@fitascsporting.ro')->subject(transex('Inregistrare pe fitascsporting.ro'));
-                }
-            );
-
-            return view("auth/login")->with(["mesaj" => ['mesaj' => transex('S-a trimis un email pentru confirmare. Trebuie sa confirmi apasand link-ul din email!')]]);
+            return view('auth/login')->with(['mesaj' => ['mesaj' => transex('S-a trimis un email pentru confirmare. Trebuie sa confirmi apasand link-ul din email!')]]);
+        } else {
+            return view('auth/register')->with(['mesaj' => ['NotOK' => $message]]);
         }
-        else{
-            return view("auth/register")->with(["mesaj" => ['NotOK' =>$message]]);
-        }
-        
     }
 
-    public function confirmregistrationemail(Request $request){
-
+    public function confirmregistrationemail(Request $request)
+    {
         $passtoken = $request->token;
 
         $confirmedEmail = '';
@@ -160,112 +131,97 @@ class LoginController extends Controller
 
         $Email = 'admin@fitascsporting.ro';
         $data = [
-            'title' => transex('Inregistrare noua pe fitascsporting.ro de catre emailul: ').$confirmedEmail,
-            'content' => transex("Apasa linkul pentru a vizualiza cererile!"),
+            'title' => transex('Inregistrare noua pe fitascsporting.ro de catre emailul: ') . $confirmedEmail,
+            'content' => transex('Apasa linkul pentru a vizualiza cererile!'),
             'link' => url('/registeries'),
-
         ];
 
-        if ($message == 'OK'){
-            Mail::send('mails.registration', $data, function($message) use ($Email){
-
+        if ($message == 'OK') {
+            Mail::send('mails.registration', $data, function ($message) use ($Email) {
                 $message->to($Email, 'User')->from('noreply@fitascsporting.ro')->subject(transex('Inregistrare noua pe fitascsporting.ro'));
-                }
-            );
+            });
 
-            return view("auth/login")->with(["mesaj" => ['mesaj' => transex('Emailul tau a fost intregistrat. Vei primi un email cand userul tau este configurat.')]]);
+            return view('auth/login')->with(['mesaj' => ['mesaj' => transex('Emailul tau a fost intregistrat. Vei primi un email cand userul tau este configurat.')]]);
+        } else {
+            return view('auth/register')->with(['mesaj' => ['NotOK' => $message]]);
         }
-        else
-            return view("auth/register")->with(["mesaj" => ['NotOK' => $message]]);
-
-
     }
-  
-    
-    public function authenticate(Request $request){
+
+    public function authenticate(Request $request)
+    {
         $parola = crypt($request->input('password'), $request->input('password'));
         $user = Login::Login($request->input('username'));
 
-        if (($request->input('password') === null) || ($request->input('username') === null)){
-            return view("auth/login")->with(["mesaj" =>[ 'password'=> transex('Introduceti emailul si parola')]]);
+        if (($request->input('password') === null) || ($request->input('username') === null)) {
+            return view('auth/login')->with(['mesaj' => ['password' => transex('Introduceti emailul si parola')]]);
         }
 
         if (!empty($user)) {
-            if ($user[0]->Password !==  $parola){
-                return view("auth/login")->with(["mesaj" =>[ 'password'=> transex('Parola incorecta')]]);
-            }
-            else{
+            if ($user[0]->Password !== $parola) {
+                return view('auth/login')->with(['mesaj' => ['password' => transex('Parola incorecta')]]);
+            } else {
                 $request->session()->put('PersonId', $user[0]->PersonId);
                 $request->session()->put('IsSuperUser', $user[0]->IsSuperUser);
                 $request->session()->put('username', $request->input('username'));
                 $request->session()->put('name', $user[0]->Name);
                 $request->session()->put('function', $user[0]->Function);
                 $request->session()->put('email', $user[0]->Email);
-                return redirect("/");
+                return redirect('/');
             }
-        }
-        else{
-            return view("auth/login")->with(["mesaj" =>[ 'password'=> transex('Email inexistent in baza de date')]]);
+        } else {
+            return view('auth/login')->with(['mesaj' => ['password' => transex('Email inexistent in baza de date')]]);
         }
     }
 
-    public function loginApi(Request $request){
-
+    public function loginApi(Request $request)
+    {
         $parola = crypt($request->password, $request->password);
         $user = $request->email;
         $device = $request->device;
 
         $user = Login::Login($user);
-       
+
         if (!empty($user)) {
-            if ($user[0]->Password !== $parola){
-                return ["status" => "Error", "Mesaj" => transex('Parola incorecta')];
-            }
-            else{
-                return ["status" => "OK",
-                    "data" => [
-                        "Token" => Login::getUserToken($user[0]->PersonId, $device),
-                        "PersonId" => $user[0]->PersonId, 
-                        "Name" => $user[0]->Name
+            if ($user[0]->Password !== $parola) {
+                return ['status' => 'Error', 'Mesaj' => transex('Parola incorecta')];
+            } else {
+                return ['status' => 'OK',
+                    'data' => [
+                        'Token' => Login::getUserToken($user[0]->PersonId, $device),
+                        'PersonId' => $user[0]->PersonId,
+                        'Name' => $user[0]->Name,
                     ]];
             }
-        }
-        else{
-            return ["status" => "Error", "Mesaj" => transex('Email inexistent')];
+        } else {
+            return ['status' => 'Error', 'Mesaj' => transex('Email inexistent')];
         }
     }
 
-    public function loginApiToken(Request $request){
-      
-        $token =$request->token;
+    public function loginApiToken(Request $request)
+    {
+        $token = $request->token;
+
         $user = Login::loginApiToken($token);
-       
+
         if (!empty($user)) {
-            return ["status" => "OK",
-                "data" => [
-                    "Token" => $token,
-                    "PersonId" => $user[0]->PersonId, 
-                    "Name" => $user[0]->Name
+            return ['status' => 'OK',
+                'data' => [
+                    'Token' => $token,
+                    'PersonId' => $user[0]->PersonId,
+                    'Name' => $user[0]->Name,
                 ]];
-            
-        }
-        else{
-            return ["status" => "Error", "Mesaj" => transex('token inexistent')];
+        } else {
+            return ['status' => 'Error', 'Mesaj' => transex('token inexistent')];
         }
     }
 
+    public function getmyusertoken(Request $request, $token)
+    {
+        $user = Login::loginApiToken($token);
 
-    public function getmyusertoken(Request $request, $token){
-         $user = Login::loginApiToken($token);
-       
-
-     
-       
         if (count($user) == 0) {
-           return redirect( "login");
-        }
-        else
-        {
+            return redirect('login');
+        } else {
             $request->session()->put('PersonId', $user[0]->PersonId);
             $request->session()->put('IsSuperUser', $user[0]->IsSuperUser);
             $request->session()->put('username', $request->username);
@@ -276,80 +232,55 @@ class LoginController extends Controller
 
             $ob = new Person;
 
-            return view('modules.pages.editables/users/myuser', ['data' => $ob->getMyUser($PersonId),  'teams' => $ob->getTeams(),  'countries' =>  Dictionary::getCountries()]);
+            return view('modules.pages.editables/users/myuser', ['data' => $ob->getMyUser($PersonId), 'teams' => $ob->getTeams(), 'countries' => Dictionary::getCountries()]);
         }
-      
     }
 
-    public function logoutApi($token){
-        return Login::logoutToken($token); 
-
+    public function logoutApi($token)
+    {
+        return Login::logoutToken($token);
     }
 
-    
-    public function logout(Request $request){
-        
-        // return dd($request->input('password'));
-        
+    public function logout(Request $request)
+    {
         $request->session()->flush();
-        
-        return redirect("welcome");
-        
-        
+        return redirect('welcome');
     }
 
-
-    public function getregisteries(){
-      
-        return view( 'auth.registeries',['master' => UserPerson::getregisteries(), 'teams' => Competition::getTeams()]);
-        
-    
+    public function getregisteries()
+    {
+        return view('auth.registeries', ['master' => UserPerson::getregisteries(), 'teams' => Competition::getTeams()]);
     }
 
-    public function finishuser(Request $request){
-
+    public function finishuser(Request $request)
+    {
         $mesaj = '';
-
         $Email = $request['Email'];
         $PersonId = $request['PersonId'];
         $RegisterId = $request['RegisterId'];
         $TeamId = $request['TeamId'];
         $Team = $request['Team'];
-
         $mesaj = UserPerson::finishuser($RegisterId, $PersonId, $Email, $TeamId, $Team);
 
-        if ($mesaj == '')
-        {
-
+        if ($mesaj == '') {
             $data = [
                 'title' => transex('Confirmare cont pe fitascsporting.ro'),
-                'content' => transex("Contul tau a fost configurat. Poti sa te loghezi urmand linkul"),
+                'content' => transex('Contul tau a fost configurat. Poti sa te loghezi urmand linkul'),
                 'link' => url('/login'),
-    
             ];
 
+            Mail::send('mails.registration', $data, function ($message) use ($Email) {
+                $message->to($Email, 'User')->from('noreply@fitascsporting.ro')->subject(transex('Confirmare cont pe fitascsporting.ro'));
+            });
 
-    
-            Mail::send('mails.registration', $data, function($message) use ($Email){
-
-                    $message->to($Email, 'User')->from('noreply@fitascsporting.ro')->subject(transex('Confirmare cont pe fitascsporting.ro'));
-                }
-            );
-
-
-                return redirect( '/registeries')->with('mesaj', $mesaj);
+            return redirect('/registeries')->with('mesaj', $mesaj);
+        } else {
+            return redirect("/registere/$RegisterId")->with('mesaj', $mesaj);
         }
-        else{
-            return redirect( "/registere/$RegisterId")->with('mesaj', $mesaj);
-        }
-
     }
 
-    public function getregistere($RegisterId ){
-        return view( 'auth.registere',['register' => UserPerson::getregistere($RegisterId), 'persons' => UserPerson::getPersonNoUser(),  'teams' => Competition::getTeams()]);
+    public function getregistere($RegisterId)
+    {
+        return view('auth.registere', ['register' => UserPerson::getregistere($RegisterId), 'persons' => UserPerson::getPersonNoUser(), 'teams' => Competition::getTeams()]);
     }
-
-    
-
-    
 }
